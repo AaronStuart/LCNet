a8ac6d146be50185995fe9f91f6f5fb5f3755234
AaronStuart <stuart543497373@gmail.com>
2020-02-25 17:39:26 +0800

    adjust apollo

--- dataset/apollo.py
+++ dataset/apollo.py
@@ -25,14 +25,8 @@ class ApolloLaneDataset(Dataset):
         return len(self.path_list)
 
     def __getitem__(self, index):
-        image_path = os.path.join(self.root_dir, self.path_list[index][0])
-        label_path = os.path.join(self.root_dir, self.path_list[index][1])
-
-        origin_image = cv2.imread(image_path)
-        origin_label = cv2.imread(label_path, cv2.IMREAD_UNCHANGED)
-
-
         ###### preprocess image #######
+        image_path = os.path.join(self.root_dir, self.path_list[index][0])
         image = Image.open(image_path)
         image_transform = transforms.Compose(
             [
@@ -40,39 +34,42 @@ class ApolloLaneDataset(Dataset):
                 transforms.ToTensor()
             ]
         )
-        image = image_transform(image)
+        input = image_transform(image)
 
         ###### preprocess label ########
+        label_path = os.path.join(self.root_dir, self.path_list[index][1])
+        origin_label = cv2.imread(label_path, cv2.IMREAD_UNCHANGED)
+
         # create a black train_id_label
         canvas = np.zeros(origin_label.shape[:2], dtype=np.uint8)
         for color, trainId in color2trainId.items():
             # map color to trainId
             mask = (origin_label == color).all(axis=2)
             canvas[mask] = trainId
-        canvas = np.expand_dims(canvas, axis = 0)
-
+        canvas = np.expand_dims(canvas, axis=0)
         label_trainId = torch.tensor(canvas)
+        
         # change from [H, W, C] to [C, H, W]
-        origin_image = np.transpose(origin_image, axes=[2, 0, 1])
+        origin_image = np.transpose(np.array(image), axes=[2, 0, 1])
         origin_label = np.transpose(origin_label, axes=[2, 0, 1])
         # change form BGR to RGB
-        origin_image = np.ascontiguousarray(origin_image[::-1, :, :])
         origin_label = np.ascontiguousarray(origin_label[::-1, :, :])
 
         return {
-            'input': image,
+            'input': input,
             'label_trainId': label_trainId,
             'origin_image': origin_image,
-            'origin_label' : origin_label
+            'origin_label': origin_label
         }
+
 
 if __name__ == '__main__':
     dataset = ApolloLaneDataset(
-        root_dir = '/media/stuart/data/dataset/Apollo/Lane_Detection',
-        path_file = '/home/stuart/PycharmProjects/EDANet/dataset/train_apollo.txt'
+        root_dir='/media/stuart/data/dataset/Apollo/Lane_Detection',
+        path_file='/home/stuart/PycharmProjects/EDANet/dataset/train_apollo.txt'
     )
     print("len of dataset: ", len(dataset))
-    
+
     data = dataset[0]
     print("input shape: ", data['input'].shape)
     print("label_trainId shape: ", data['label_trainId'].shape)
