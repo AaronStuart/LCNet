80ba28d13c0b4d25bc825c9156c275e030e8b3aa
Aaron Stuart <stuart543497373@gmail.com>
2020-05-05 21:46:21 +0800

    finish visualize embedding

--- scripts/visualize_train.py
+++ scripts/visualize_train.py
@@ -59,3 +59,12 @@ class TrainVisualize:
             label_channel = logits[trainId].unsqueeze(dim=0).unsqueeze(dim=0)
             self.summary.add_image('origin_logits/%s' % label_name, vutils.make_grid(label_channel, normalize=True),
                                    iteration)
+
+        # visualize foreground's embedding
+        C, H, W = logits.shape
+        flatten_label = label.view(-1).numpy()
+        self.summary.add_embedding(
+            mat = logits.permute(1, 2, 0).view(-1, C)[flatten_label != 0],
+            metadata = list(map(lambda x : trainId2name[x], flatten_label[[flatten_label != 0]])),
+            global_step = iteration
+        )
